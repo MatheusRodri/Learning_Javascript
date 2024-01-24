fa987cb3373ece5d3cb80160fe633dc4bff55cc6
Matheus <matheus.rj25@hotmail.com>
2024-01-24 19:44:30 -0300

    add example 3

---
+++ js/example3.js
@@ -0,0 +1,32 @@
+var lista = ['Matheus','Rodrigues',25]
+lista
+
+lista[0]
+lista.indexOf('Matheus')
+lista.indexOf(0)
+
+lista[2] = 44
+
+lista.push('São Paulo')
+lista.push('SP')
+lista.push('Brasil')
+lista.push('America do Sul')
+lista.push('Terra')
+lista.push('Via Láctea')
+lista.push('Universo')
+lista.push('Multiverso')
+lista.push('Omniverso')
+lista.push('Omnitrix')
+lista.push('Ben 10')
+lista.push('Ben 10: Força Alienígena')
+lista.push('Ben 10: Supremacia Alienígena')
+lista.push('Ben 10: Omniverse')
+lista.push('Ben 10: Reboot')
+
+lista
+
+lista.shift()
+lista.pop()
+
+
+lista.join(';')
